b6c7dfc174998555046e14da7f79261542231dc0
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-02 17:33:33 +0300

    changes
    Added items in pipeline
    added UTF 8 in settings
    created .json file with dictionary for parties short names -> 'parties_dictionary.json'
    spyder formatting
    deleted quotes
    results.js - first file with profile results UTF-8

--- src/parliamentbg/parliamentbg/items.py
+++ src/parliamentbg/parliamentbg/items.py
@@ -8,5 +8,16 @@ import scrapy
 
 class ParliamentbgItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    date_born = scrapy.Field()
+    place_born = scrapy.Field()
+    profession = scrapy.Field()
+    lang = scrapy.Field()
+    party = scrapy.Field()
+    email = scrapy.Field()
+    url = scrapy.Field()
+    education = scrapy.Field()
+    pp = scrapy.Field()
+    dob = scrapy.Field()
     pass
+
--- src/parliamentbg/parliamentbg/spiders/spyder.py
+++ src/parliamentbg/parliamentbg/spiders/spyder.py
@@ -1,19 +1,18 @@
-from scrapy.selector import Selector
-
 import scrapy
+from scrapy.selector import Selector
 
 
 class QuotesSpider(scrapy.Spider):
     name = "parliament_all"
     start_urls = [
-        'https://parliament.bg/bg/MP',
+        "https://parliament.bg/bg/MP",
     ]
 
     def parse(self, response):
         selector = Selector(response)
         urls_short = selector.xpath("//div[3]/div/div/div/a/@href").getall()
-        start_urls = [
-            "https://www.parliament.bg" + short for short in urls_short
-        ]
-        yield {'urls': start_urls, }
-
+        start_urls = ["https://www.parliament.bg" + short for short in
+                      urls_short]
+        yield {
+            "urls": start_urls,
+        }
