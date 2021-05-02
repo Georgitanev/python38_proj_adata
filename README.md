4bccfefe995cab5c776d49ab652cd76212d59ca3
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-02 13:59:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python38_projadata
+ sraper
---
+++ src/parliamentbg/parliamentbg/spiders/spyder.py
@@ -0,0 +1,19 @@
+from scrapy.selector import Selector
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "parliament_all"
+    start_urls = [
+        'https://parliament.bg/bg/MP',
+    ]
+
+    def parse(self, response):
+        selector = Selector(response)
+        urls_short = selector.xpath("//div[3]/div/div/div/a/@href").getall()
+        start_urls = [
+            "https://www.parliament.bg" + short for short in urls_short
+        ]
+        yield {'urls': start_urls, }
+
