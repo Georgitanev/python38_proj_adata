9d185b48cdfa53257ad1e41ea4ad6a5e23294aa6
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-03 10:49:34 +0300

    New dict generator with new parties and generated json

--- dict_to_json.py
+++ dict_to_json.py
@@ -1,13 +1,24 @@
 import json
+# da vpi6a novite partii
+# parties_dictionary = {
+#     "ПП ГЕРБ": "GERP",
+#     "БСП за БЪЛГАРИЯ": "BSP",
+#     "ВОЛЯ": "VOLQ",
+#     "Движение за права и свободи - ДПС": "DPS",
+#     "ОБЕДИНЕНИ ПАТРИОТИ - НФСБ, АТАКА и ВМРО": "OP",
+# }
+# with open('parties_dictionary.json'.format(1), 'w', encoding='utf-8') as file:
+#     json.dump(parties_dictionary, file, ensure_ascii=False)
 
-parties_dictionary = {
-    "ПП ГЕРБ": "GERP",
+#//TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+parties_dictionary_new = {
+    "ГЕРБ-СДС": "GERP",
+    "ПП ИМА ТАКЪВ НАРОД": "ITN",
     "БСП за БЪЛГАРИЯ": "BSP",
-    "ВОЛЯ": "VOLQ",
     "Движение за права и свободи - ДПС": "DPS",
-    "ОБЕДИНЕНИ ПАТРИОТИ - НФСБ, АТАКА и ВМРО": "OP",
+    "ДЕМОКРАТИЧНА БЪЛГАРИЯ": "DB",
+    "ИЗПРАВИ СЕ! МУТРИ ВЪН!": "IMV",
 }
 
-with open('parties_dictionary.json'.format(1), 'w', encoding='utf-8') as file:
-    json.dump(parties_dictionary, file, ensure_ascii=False)
-
+with open('parties_dictionary_new.json'.format(1), 'w', encoding='utf-8') as file:
+    json.dump(parties_dictionary_new, file, ensure_ascii=False)
