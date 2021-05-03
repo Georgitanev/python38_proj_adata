4b8fa51bd22bc5fa3db92e1db94758011835be1f
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-03 13:45:12 +0300

    added json schema validation
    dictionary json is opening by json, not by pandas (pandas have some warning message).
    added json_schema with email validation.
    added new dictionary.
    yield items only
    url - whole url - removed [-11:]

--- src/parliamentbg/parliamentbg/spiders/profile_page.py
+++ src/parliamentbg/parliamentbg/spiders/profile_page.py
@@ -1,8 +1,10 @@
+import json
 import os
 
 import pandas as pd
 import scrapy
 from scrapy.selector import Selector
+from scrapy_jsonschema.item import JsonSchemaItem
 
 from ..items import ParliamentbgItem
 
@@ -10,10 +12,25 @@ from ..items import ParliamentbgItem
 
 # opening short names party dictionary
 path = os.path.dirname(os.path.abspath(__file__))
-parties_dictionary_file = os.path.join(path, "parties_dictionary.json")
-xls_url = os.path.join(path, "MemberofParliament2797.xls")
-parties_dictionary_df = pd.read_json(parties_dictionary_file, "r", "utf-8")
-parties_dictionary = parties_dictionary_df.to_dict()
+parties_dictionary_file = os.path.join(path, "parties_dictionary_new.json")
+schema_path = os.path.join(path, "json_schema_with_email.json")
+
+
+# keeping this until finish the script in case of website not working again
+# xls_url = os.path.join(path, "MemberofParliament2797.xls")
+# Load json schema for validation
+
+
+def open_json(schema_path):
+    with open(schema_path, encoding='utf-8-sig') as fh:
+        return json.load(fh)
+
+
+parties_dictionary = open_json(parties_dictionary_file)
+
+
+class ProductItem(JsonSchemaItem):
+    jsonschema = open_json(schema_path)
 
 
 def extract_political_force(party=""):
@@ -30,27 +47,25 @@ class QuotesSpider(scrapy.Spider):
     """Crawler for profile page with personal information"""
 
     name = "profile_page"
-    start_urls = ['https://www.abv.bg/',
-        # "https://parliament.bg/bg/MP/3013",
-    ]
+    # use this url in case website not working again 'https://www.abv.bg/',
+    start_urls = ["https://parliament.bg/bg/MP/3013", ]
 
     def parse(self, response, **kwargs):
         items = ParliamentbgItem()
         # temporary not using parliament, because website not working
         selector = Selector(response)
-        # xls_url = selector.xpath(
-        #     '//*[@id="leftcontent-2"]/div[3]/div[2]/a[2]/@href'
-        # ).get()
-        #
-        # xls_url = "https://parliament.bg" + xls_url
+        xls_url = selector.xpath(
+            '//*[@id="leftcontent-2"]/div[3]/div[2]/a[2]/@href'
+        ).get()
 
+        xls_url = "https://parliament.bg" + xls_url
         # temporary because website not working
         df = pd.read_excel(xls_url)  # reading .xls from url
         df = df.where(pd.notnull(df), None)
         names = df.columns[1]  # get the name of column with names
         # get date of birth
         date_of_birth = \
-        df.loc[df["Unnamed: 0"] == "Дата на раждане ", names].tolist()[0]
+            df.loc[df["Unnamed: 0"] == "Дата на раждане ", names].tolist()[0]
         try:
             place_of_birth = df["Unnamed: 2"][0] + " " + df["Unnamed: 3"][0]
         except Exception as ex:
@@ -86,11 +101,9 @@ class QuotesSpider(scrapy.Spider):
         items["lang"] = foreign_languages
         items["party"] = party
         items["email"] = email
-        items["url"] = response.request.url[-11:]
+        items["url"] = response.request.url  # [-11:]
         items["education"] = education
         items["pp"] = parties_dictionary[party]  # political party short version
         # date of birth short version
         items["dob"] = date_of_birth.replace("-", "")
-        yield {
-            "urls": items,
-        }
+        yield items
