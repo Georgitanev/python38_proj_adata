b535c975a619802daa6fa5b6cb02e2731581dd54
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-03 13:47:31 +0300

    added json schma and saving into file

--- dict_to_json.py
+++ dict_to_json.py
@@ -22,3 +22,86 @@ parties_dictionary_new = {
 
 with open('parties_dictionary_new.json'.format(1), 'w', encoding='utf-8') as file:
     json.dump(parties_dictionary_new, file, ensure_ascii=False)
+
+# schema save to json file
+schema_json = {'$schema': 'http://json-schema.org/schema#',
+        'type': 'object', 'properties':
+            {
+                "name": {
+                    "type": "string"
+                },
+                "date_born": {
+                    "type": "string"
+                },
+                "place_born": {
+                    "anyOf": [
+                        {
+                            "type": "null"
+                        },
+                        {
+                            "type": "string"
+                        }
+                    ]
+                },
+                "profession": {
+                    "anyOf": [
+                        {
+                            "type": "null"
+                        },
+                        {
+                            "type": "string"
+                        }
+                    ]
+                },
+                "lang": {
+                    "anyOf": [
+                        {
+                            "type": "null"
+                        },
+                        {
+                            "type": "string"
+                        }
+                    ]
+                },
+                'party': {'type': 'string'},
+                "email": {
+                    "title": "Email address",
+                    "type": "string",
+                    "pattern": "^\\S+@\\S+\\.\\S+$",
+                    "format": "email",
+                    "minLength": 6,
+                    "maxLength": 127
+                },
+                'url': {'type': 'string'},
+                'education': {
+                    "anyOf": [
+                        {
+                            "type": "null"
+                        },
+                        {
+                            "type": "string"
+                        }
+                    ]
+                },
+                'pp': {'type': 'string'},
+                'dob': {'type': 'string'},
+            },
+        "required": [
+            "date_born",
+            "dob",
+            "education",
+            "email",
+            "lang",
+            "name",
+            "party",
+            "place_born",
+            "pp",
+            "profession",
+            "url"
+        ],
+        "title": "Welcome"
+    }
+
+with open('json_schema_with_email.json'.format(1), 'w', encoding='utf-8') as file:
+    json.dump(schema_json, file, ensure_ascii=False)
+
