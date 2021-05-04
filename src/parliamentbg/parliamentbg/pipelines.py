9c29780f34f15bff77c83d70aca32dcfbd3c5f9d
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-04 14:16:01 +0300

    Pipeline for data - create table in db and adding info in table parliament

--- src/parliamentbg/parliamentbg/pipelines.py
+++ src/parliamentbg/parliamentbg/pipelines.py
@@ -1,13 +1,46 @@
+from sqlalchemy.orm import sessionmaker
+
+from .models import Parliament
+from .models import create_table
+from .models import db_connect
+
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
 
-class ParliamentbgPipeline:
+class ParliamentbgPipeline(object):
+    """Livingsocial pipeline for storing scraped items in the database"""
+
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates deals table.
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
     def process_item(self, item, spider):
+        """Save deals in the database.
+
+        This method is called for every item pipeline component.
+
+        """
+        session = self.Session()
+        deal = Parliament(**item)
+
+        try:
+            session.add(deal)
+            session.commit()
+        except Exception as ex:
+            print(ex)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item
