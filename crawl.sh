a90b3546d1e833e753f118d389c5a394df3f67b5
Georgi Tanev <georgi.tanev86@gmail.com>
2021-05-14 13:41:15 +0300

    added .sh script to run scrapy under linux in venv

---
+++ crawl.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+# go to the spider directory
+cd src/parliamentbg/
+
+# getting working directory and print it
+CWD="$(pwd)"
+echo $CWD
+
+# get date time for testing purposes
+# datetime=$(date '+%m_%d_%Y_%H_%M_%S')
+# run the spider
+#scrapy crawl multi_subject_spider -a debug='True' &> "logs/log_${datetime}.txt"
+
+/usr/local/bin/scrapy crawl parliament_all
